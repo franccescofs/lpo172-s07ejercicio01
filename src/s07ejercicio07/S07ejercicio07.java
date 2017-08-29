b5c4143878982a4a2d83580e7098cea86603007a
alumno <alumno@LIMSFLAB06-15D.system-uap.adm>
2017-08-28 21:34:15 -0500

    first commit

---
+++ src/s07ejercicio07/S07ejercicio07.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s07ejercicio07;
+
+/**
+ *
+ * @author alumno
+ */
+public class S07ejercicio07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Creando objeto Scanner
+        Scanner input = new Scanner(System.in);
+        
+        // Mostrando mensaje al usuario
+        System.out.print("Ingrese la distancia a recorrer: ");
+        double distancia = input.nextDouble();
+        
+        System.out.println("Ingrese las millas por galon");
+        double millasporgalon = input.nextDouble();
+        
+        System.out.println("Ingrese precio de galon")
+        
+    }
+    
+}
